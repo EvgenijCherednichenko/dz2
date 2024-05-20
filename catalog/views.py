90ba1ab346d37b5a32d927030eb3be937b51c206
EvgenijCherednichenko <Ch_evgenij@mail.ru>
2024-05-20 17:59:26 +0300

    Поправил код...

--- catalog/views.py
+++ catalog/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+def hom(request):
+    return render(request, 'contacts.html')
