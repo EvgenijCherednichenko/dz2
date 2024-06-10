2af3831cab9d49ee35f8d8adc59a8b93adf08bdc
EvgenijCherednichenko <Ch_evgenij@mail.ru>
2024-06-10 22:04:56 +0300

    Исправил что-то...

--- product/admin.py
+++ product/admin.py
@@ -4,12 +4,11 @@ from product.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", 'price', 'category')
+    list_display = ("id", 'name', 'price', 'category', 'description')
     list_filter = ('category',)
-    search_fields = ('name',)
+    search_fields = ('name', 'description',)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", 'name')
-    search_fields = ('name',)
