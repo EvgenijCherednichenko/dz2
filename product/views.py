748dce276ad57b44bc0692ca30f43499a89fb87e
EvgenijCherednichenko <Ch_evgenij@mail.ru>
2024-06-18 00:27:52 +0300

    20.2 Шаблонизация

--- product/views.py
+++ product/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from product.models import Product
+
+
+def index(request):
+    product = Product.objects.all()
+    context = {"product": product}
+    return render(request, 'product_sample.html', context)
+
+
+def product_detail(request, pk):
+    prod = get_object_or_404(Product, pk=pk)
+    context = {"prod": prod}
+    return render(request, 'product_detail.html', context)
